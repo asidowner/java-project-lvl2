32b3ad191588fe8139b62ee2ca0034093c75c01f
Anton Sidorenkov <asid.owner@gmail.com>
2022-05-12 20:39:04 +0400

    step #6.3

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -3,8 +3,6 @@ package hexlet.code;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DifferTest {
 
     @BeforeEach
